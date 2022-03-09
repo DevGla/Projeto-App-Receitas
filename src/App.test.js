0029226ea67012390eb671f7b9f0f347272f7db0
george lucas <glalmeida.dev@gmail.com>
2022-03-09 20:15:42 -0300

    Requitos-telas-explorar-explorarComidasBebidas-perfil

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,45 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Tela de Login', () => {
+  it('', () => {});
+});
+describe('Header', () => {
+  it('', () => {});
+});
+describe('Barra de Busca', () => {
+  it('', () => {});
+});
+describe('Menu Inferior', () => {
+  it('', () => {});
+});
+describe('Tela principal de receitas', () => {
+  it('', () => {});
+});
+describe('Tela de detalhes de uma receita', () => {
+  it('', () => {});
+});
+describe('Tela de receita em progresso', () => {
+  it('', () => {});
+});
+describe('Tela de receitas feitas', () => {
+  it('', () => {});
+});
+describe('Tela de receitas favoritas', () => {
+  it('', () => {});
+});
+describe('Tela de explorar', () => {
+  it('', () => {});
+});
+describe('Tela de explorar bebidas ou comidas', () => {
+  it('', () => {});
+});
+describe('Tela de explorar ingredientes', () => {
+  it('', () => {});
+});
+describe('Tela de explorar por nacionalidades', () => {
+  it('', () => {});
+});
+describe('Tela de Perfil', () => {
+  it('', () => {});
 });
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const history = useHistory();
+  const [stateEmail, setStateEmail] = useState('');
+
+  useEffect(() => { setStateEmail(localStorage.getItem('user')); }, []);
+
   return (
-    <>
-      <h1 data-testid="page-title">Profile</h1>
+    <div>
       <Header />
+      <h1 data-testid="page-title">Profile</h1>
+      <p data-testid="profile-email">{stateEmail}</p>
+      <Link to="/done-recipes" data-testid="profile-done-btn">Done Recipes</Link>
+      <Link
+        to="/favorite-recipes"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          history.push('/');
+          localStorage.clear();
+        } }
+      >
+        Logout
+      </button>
       <Footer />
-    </>
+    </div>
   );
 }
 
